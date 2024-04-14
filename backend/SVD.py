9c73d3d4e37a03fa5e493b66e23307162280b9a6
Crystal Zhu <cyz22@cornell.edu>
2024-04-13 23:31:16 -0400

    return full documents from SVD instead of descriptions only

--- backend/SVD.py
+++ backend/SVD.py
@@ -10,18 +10,15 @@ df = pd.read_csv("data/coffee_fix.csv")
 df['desc_all'] = df['desc_1'] + '\n' + df['desc_2'] + '\n' + df['desc_3']
 df['desc_all'] = df['desc_all'].astype(str)
 
+documents = df.values.tolist()
+
+print(df.info())
+
 vectorizer = TfidfVectorizer(stop_words = 'english', min_df = 70, max_df = 0.7)
-td_matrix = vectorizer.fit_transform(df['desc_all'].to_list())
+td_matrix = vectorizer.fit_transform([x[-1] for x in documents]) # combined description is last column in df
 
 docs_compressed, s, words_compressed = svds(td_matrix, k=20)
 words_compressed = words_compressed.transpose()
-
-''' Show that most data lives in 20 dimensions
-plt.plot(s[::-1])
-plt.xlabel("Singular value number")
-plt.ylabel("Singular value")
-plt.show()
-'''
 
 word_to_index = vectorizer.vocabulary_
 index_to_word = {i:t for t,i in word_to_index.items()}
@@ -53,19 +50,20 @@ print()
 
 docs_compressed_normed = normalize(docs_compressed)
 
-documents = df['desc_all'].to_list()
 def closest_docs(doc_index_in, doc_repr_in, k = 5):
     sims = doc_repr_in.dot(doc_repr_in[doc_index_in,:])
     asort = np.argsort(-sims)[:k+1]
-    return [(documents[i],sims[i]) for i in asort[1:]]
+    # Index 4 is name
+    return [(documents[i][4], documents[i][-1], sims[i]) for i in asort[1:]]
 
 for i in range(5):
-    print("INPUT DESCRIPTION: "+documents[i])
+    print("INPUT NAME: "+documents[i][4])
+    print("INPUT DESCRIPTION: "+documents[i][-1])
     print()
     print("CLOSEST DESCRIPTIONS:")
     print("Using SVD:")
-    for desc, score in closest_docs(i, docs_compressed_normed):
-        print("{}:{:.3f}".format(desc, score))
+    for name, desc, score in closest_docs(i, docs_compressed_normed):
+        print("{}: {:.3f} \n {}".format(name, score, desc))
         print()
     print("--------------------------------------------------------\n")
 
@@ -75,23 +73,23 @@ def closest_docs_to_word(word_in, k = 5):
     if word_in not in word_to_index: return "Not in vocab."
     sims = docs_compressed_normed.dot(words_compressed_normed[word_to_index[word_in],:])
     asort = np.argsort(-sims)[:k+1]
-    return [(i, documents[i],sims[i]) for i in asort[1:]]
+    return [(i, documents[i][4], documents[i][-1], sims[i]) for i in asort[1:]]
 
-for i, desc, sim in closest_docs_to_word("fruity"):
-    print("{}\n {}\n {:.4f}".format(i, desc, sim))
+for i, name, desc, sim in closest_docs_to_word("fruity"):
+    print("{}\n {}\n {}\n {:.4f}".format(i, name, desc, sim))
 
 print ("-------------------")
+
+# Query to doc
+
 query = "Chocolaty and nutty with floral notes, creamy mouthfeel and a hint of tartness"
 query_tfidf = vectorizer.transform([query]).toarray()
-query_tfidf.shape
-query_tfidf
-words_compressed.shape
 query_vec = normalize(np.dot(query_tfidf, words_compressed)).squeeze()
-query_vec.shape
+
 def closest_docs_to_query(query_vec_in, k = 5):
     sims = docs_compressed_normed.dot(query_vec_in)
     asort = np.argsort(-sims)[:k+1]
-    return [(i, documents[i],sims[i]) for i in asort[1:]]
+    return [(i, documents[i][4], documents[i][-1],sims[i]) for i in asort[1:]]
 
-for i, desc, sim in closest_docs_to_query(query_vec):
-    print("({}, {}, {:.4f}".format(i, desc, sim))
+for i, name, desc, sim in closest_docs_to_query(query_vec):
+    print("({}\n {}\n {}\n {:.4f}".format(i, name, desc, sim))
