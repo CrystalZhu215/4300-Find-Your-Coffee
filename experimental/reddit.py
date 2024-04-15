23255a17501cdba7775297147739f5dd0e19cc1c
bnw36 <bnw36@cornell.edu>
2024-04-15 08:44:25 -0400

    Changed reddit.py

--- experimental/reddit.py
+++ experimental/reddit.py
@@ -18,13 +18,14 @@ import pprint
 reddit = praw.Reddit(
     client_id="your client id",
     client_secret="your client secret",
-    user_agent="macos:findyourcoffee:v1.0.0 (by u/worm-dealer)"
+    user_agent="macos:findyourcoffee:v1.0.0 (by u/worm-dealer)",
 )
 print(reddit.read_only)
 
 # Show listings (general): top_ten_results = [s for s in reddit.subreddit("coffee").new(limit=10)]
 
 # Search a subreddit using a query:
+
 
 def find_query_in_comments(query, comment):
 
@@ -34,29 +35,33 @@ def find_query_in_comments(query, comment):
 
         results = []
         lower_text = reply.body.lower()
-        lines = lower_text.split('. ')
+        lines = lower_text.split(". ")
         for line in lines:
             if query in line:
                 results.append(line)
 
         for r in reply.replies:
             results += find_query_in_comments_acc(r)
-        
+
         return results
 
     return find_query_in_comments_acc(comment)
 
+
 def preprocess_text(text):
 
     tokens = word_tokenize(text.lower())
-    filtered_tokens = [token for token in tokens if token not in stopwords.words('english')]
+    filtered_tokens = [
+        token for token in tokens if token not in stopwords.words("english")
+    ]
 
     lemmatizer = WordNetLemmatizer()
     lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
 
-    processed_text = ' '.join(lemmatized_tokens)
+    processed_text = " ".join(lemmatized_tokens)
 
     return processed_text
+
 
 # query issue: 'press coffee' -> 'french press coffee'
 query = "modcup"
@@ -66,7 +71,7 @@ search_results = [s for s in reddit.subreddit("coffee").search(query=query)]
 query_found = []
 for submission in search_results:
     lower_text = submission.selftext.lower()
-    lines = '. '.join(lower_text.split('\n')).split('. ')
+    lines = ". ".join(lower_text.split("\n")).split(". ")
     for line in lines:
         if query in line:
             query_found.append(line)
@@ -76,11 +81,13 @@ for submission in search_results:
         query_found += find_query_in_comments(query, c)
 
 for i, result in enumerate(query_found):
-    print(str(i+1) + ')', result)
+    print(str(i + 1) + ")", result)
 
 analyzer = SentimentIntensityAnalyzer()
 
+stored = set()
 for text in query_found:
     processed_text = preprocess_text(text)
-    sentiments = analyzer.polarity_scores(text)
+    sentiments = analyzer.polarity_scores(processed_text)
     print(sentiments)
+    stored[text] = sentiments
